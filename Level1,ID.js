2ec3439e5a4bc1649118bee62f7e62037dc0adde
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-06-26 15:21:12 +0900

    Create Level1,ID.js
    카카오에 입사한 신입 개발자 네오는 "카카오계정개발팀"에 배치되어, 카카오 서비스에 가입하는 유저들의 아이디를 생성하는 업무를 담당하게 되었습니다. "네오"에게 주어진 첫 업무는 새로 가입하는 유저들이 카카오 아이디 규칙에 맞지 않는 아이디를 입력했을 때, 입력된 아이디와 유사하면서 규칙에 맞는 아이디를 추천해주는 프로그램을 개발하는 것입니다.

---
+++ Level1,ID.js
@@ -0,0 +1,63 @@
+const solution = id => {
+  id = id.toLowerCase()
+  id = removeInvalidChar(id)
+  id = removeInvalidDots(id)
+
+  if (!id) {
+    id = 'a'
+  }
+
+  if (id.length > 15) {
+    id = id.slice(0, 15)
+  }
+
+  id = removeInvalidDots(id)
+
+  if (id.length < 3) {
+    let lastChild = id.charAt(id.length - 1)
+
+    do {
+      id += lastChild
+    }
+    while (id.length === 2)
+  }
+
+  return id
+}
+
+const removeStrIndex = (str, index) => {
+  let result = str.split('')
+  result.splice(index, 1)
+
+  return result.join('')
+}
+
+const removeInvalidChar = str => {
+  for (let i = 0; i < str.length; i++) {
+    if (!(
+      str.charCodeAt(i) === 45 ||
+      str.charCodeAt(i) === 46 ||
+      str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57 ||
+      str.charCodeAt(i) === 95 ||
+      str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122
+    )) {
+      str = str.replace(str[i], '')
+      i--
+    }
+  }
+
+  return str
+}
+
+const removeInvalidDots = str => {
+  do {
+    str = str.replace('..', '.')
+  }
+  while (str.indexOf('..') > -1)
+
+  if (str.startsWith('.')) {
+    str = removeStrIndex(str, 0)
+  }
+  if (str.endsWith('.')) {
+    str = removeStrIndex(str, str.length - 1)
+  }

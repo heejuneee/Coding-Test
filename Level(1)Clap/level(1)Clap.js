c50b0b332c005268cfaa943366831483da623e16
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-07-04 15:14:12 +0900

    Create level(1)Clap.js

---
+++ Level(1)Clap/level(1)Clap.js
@@ -0,0 +1,10 @@
+function waterMelon(n){
+  var result = "수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박"
+  //함수를 완성하세요
+
+  return result.substring(0,n);
+}
+
+// 실행을 위한 테스트코드입니다.
+console.log("n이 3인 경우: "+ waterMelon(3))
+console.log("n이 4인 경우: "+ waterMelon(4))
